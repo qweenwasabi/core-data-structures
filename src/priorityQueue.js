9bdaa1c8372cf8680e8248de47f6bed768b6bb06
qweenwasabi <jacklynebouier@Jacklyne-Bouiers-MacBook-Pro.local>
2017-02-21 06:30:24 -0800

    Set and PriorityQueue added without test

--- src/priorityQueue.js
+++ src/priorityQueue.js
@@ -1,5 +1,33 @@
+'use strict'
 
   export default class PriorityQueue {
   constructor() {
-    this.queue = [];
+    this.pQueue = [];
   }
+
+  enqueue(value) {
+    this.pQueue.push(value);
+  }
+
+  front() {
+    return this.pQueue[0];
+  }
+
+  back() {
+  return this.pQueue[this.pQueue.length-1];
+  }
+
+  dequeue() {
+  return this.pQueue.shift();
+  }
+  isEmpty(){
+   if (this.pQueue.length == 0) {
+    return true;}
+  }
+  length() {
+    return this.pQueue.length;
+  }
+  print() {
+    console.log(this.pQueue.join(' '));
+  }
+}
